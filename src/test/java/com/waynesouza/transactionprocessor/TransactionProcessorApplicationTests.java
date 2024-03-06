52e2f409efbd9fbdb1a780cadd3026a5c914830c
waynesouza <waynesouza07@gmail.com>
2024-03-06 18:59:18 -0300

    feat: initial commit

---
+++ src/test/java/com/waynesouza/transactionprocessor/TransactionProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waynesouza.transactionprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
